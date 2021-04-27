3e6d764bae14b798c86193bc5749eccc306cec02
YI FANG YUAN <yi.f.yuan@mail.mcgill.ca>
2021-04-26 22:36:05 -0400

    fix responsiveness

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -5,37 +5,37 @@ import Col from 'react-bootstrap/Col';
 export default function Experience() {
 
     return (
-        <Container fluid className="experience" id="experience">
+        <div className="experience" id="experience">
 
             <h1>EXPERIENCE</h1>
             <div className="purpleBox"></div>
-            <Row lg={12} className="workExperience">
-                <Col lg={6} xs={12}><h3>Morgan Stanley - IST Division</h3>
+            <div className="workExperience">
+                <div><h3>Morgan Stanley - IST Division</h3>
                     <h4>Technology Analyst Intern  </h4>
                     <h5>January 2021 - August 2021</h5>
-                </Col>
-                <Col lg={6} xs={12}>
+                </div>
+                <div className="workDetails">
                     <ul>
                         <li>Implemented an automatic transaction processing for the transfer of non-accruing loans<br /><br /></li>
                         <li>Built user interface in <b>React/TypeScript</b> to allow Global Loans Services Operations Team to review and trigger automation for selected loans<br /><br /></li>
                         <li>Built backend web service logic using <b>Spring Boot & SQL</b> to execute automatic booking of trades, journals, and wire transfers to reduce loan transaction  from <b>2 hours to few minutes</b><br /><br /></li>
                         <li>Practiced <b>continuous integration and SDLC process</b> to deploy new feature on Production</li>
                     </ul>
-                </Col>
-            </Row>
-            <Row lg={12} className="workExperience">
-                <Col lg={6} xs={12} ><h3>Nebula AI</h3>
+                </div>
+            </div>
+            <div className="workExperience">
+                <div><h3>Nebula AI</h3>
                     <h4>Front End Developer Intern  </h4>
                     <h5>July 2020 – Sep 2020</h5>
-                </Col>
-                <Col lg={6} xs={12}>
+                </div>
+                <div className="workDetails">
                     <ul>
                         <li>Created a responsive web application using <b>HTML, CSS and Vue</b> to sell computing cloud solution<br /><br /></li>
                         <li>Decreased <b>loading time</b> of website by <b>over 30%</b> using lazy loading feature in Vue<br /><br /></li>
                         <li>Added a lightweight translation module (i18n) to co-worker’s project, which allowed users to view the website in multiple languages<br /><br /></li>
                     </ul>
-                </Col>
-            </Row>
-        </Container >
+                </div>
+            </div>
+        </div >
     )
 }
--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -1,20 +1,47 @@
-import Col from 'react-bootstrap/Col';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
 import './Project.scss'
 export default function Project() {
 
+    const languageStyle = {
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "1.5rem",
+        padding: "10px 10px 10px 10px",
+        borderRadius: "20.9381px",
+    }
+
+    const chooseSkillColor = (params) => {
+        let dummy = Object.assign({}, languageStyle);
+        switch (params) {
+            case 'javascript':
+                dummy.backgroundColor = "#ffb37b";
+                return dummy;
+
+            case 'react':
+                dummy.backgroundColor = "#a7d7f2";
+                return dummy;
+
+            case 'vue':
+                dummy.backgroundColor = "#00ff7f";
+                return dummy;
+
+            case 'express':
+                dummy.backgroundColor = "#A0522D";
+                return dummy;
+            case 'typescript':
+                dummy.backgroundColor = "#66CDAA";
+                return dummy;
+        }
+    }
     return (
-        <Container fluid className="projects" id="projects">
+        <div className="projects" id="projects">
 
             <h1>PROJECTS</h1>
             <div className="purpleBox"></div>
-            <Row xs={12} lg={7} className="project">
-                <Col md={6} xs={12} className="project-pic">
-                    <a target="_blank" href="https://yiyi-covid19-tracker.herokuapp.com/"><img src="https://user-images.githubusercontent.com/52011886/116006886-290beb80-a5db-11eb-95a8-1d127a00c43b.png" alt="true"></img>
-                    </a>
-                </Col>
-                <Col md={6} xs={12}>
+            <div className="project">
+                <a target="_blank" href="https://yiyi-covid19-tracker.herokuapp.com/"><img className="project-pic" src="https://user-images.githubusercontent.com/52011886/116006886-290beb80-a5db-11eb-95a8-1d127a00c43b.png" alt="true"></img>
+                </a>
+                <div className="description">
                     <h4>Covid Tracker</h4>
                     <a target="_blank" href="https://yiyi-covid19-tracker.herokuapp.com/"><h3>COVID-19-TRACKER</h3>
                     </a>
@@ -24,18 +51,18 @@ export default function Project() {
                         <li>Built backend web service logic using <b>Spring Boot & SQL</b> to execute automatic booking of trades, journals, and wire transfers to reduce loan transaction  from <b>2 hours to few minutes</b><br /><br /></li>
                         <li>Practiced <b>continuous integration and SDLC process</b> to deploy new feature on Production</li>
                     </ul>
-                    <Row className="languages">
-                        <Col md={2} xs={2} className="React"><h5>VUE</h5></Col>
-                        <Col md={{ span: 3 }} xs={4} className="JavaScript"><h5>JAVASCRIPT</h5></Col>
-                    </Row>
-                </Col>
-            </Row >
-            <Row xs={12} lg={9} className="project">
-                <Col md={6} xs={12} className="project-pic">
-                    <a target="_blank" href="https://mumap.xyz/"><img src="https://user-images.githubusercontent.com/52011886/116021151-9f2d4400-a615-11eb-8b21-c1d0c42c176b.png" alt="true"></img>
+                    <div className="languages">
+                        <div style={chooseSkillColor("vue")}><h5>VUE</h5></div>
+                        <div style={chooseSkillColor("javascript")}><h5>JAVASCRIPT</h5></div>
+                    </div>
+                </div>
+            </div >
+            <div className="project">
+                <div className="picture">
+                    <a target="_blank" href="https://mumap.xyz/"><img className="project-pic" src="https://user-images.githubusercontent.com/52011886/116021151-9f2d4400-a615-11eb-8b21-c1d0c42c176b.png" alt="true"></img>
                     </a>
-                </Col>
-                <Col md={6} xs={12}>
+                </div>
+                <div className="description">
                     <h4>Open-Air Museum for Artists</h4>
                     <a target="_blank" href="https://mumap.xyz/"><h3>MU MAP</h3>
                     </a>
@@ -45,12 +72,14 @@ export default function Project() {
                         <li>Created visual display of search result from <b>100 + murals</b> in database using <b>React and TypeScript</b><br /><br /></li>
                         <li>Implemented <b>instant rendering </b> to avoid reloading the whole website for newly created murals on the webpage using <b>React Context</b></li>
                     </ul>
-                    <Row className="languages">
-                        <Col md={2} xs={2} className="JavaScript"><h5>VUE</h5></Col>
-                        <Col md={{ span: 3 }} xs={4} className="JavaScript"><h5>JAVASCRIPT</h5></Col>
-                    </Row>
-                </Col>
-            </Row >
-        </Container >
+                    <div className="languages">
+                        <div style={chooseSkillColor("react")}><h5>REACT</h5></div>
+                        <div style={chooseSkillColor("javascript")}><h5>JAVASCRIPT</h5></div>
+                        <div style={chooseSkillColor("typescript")}><h5>TYPESCRIPT</h5></div>
+                        <div style={chooseSkillColor("express")}><h5>EXPRESS.JS</h5></div>
+                    </div>
+                </div>
+            </div >
+        </div >
     )
 }
