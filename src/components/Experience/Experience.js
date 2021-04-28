0463415ed1133b16c66f966a40bbef3ca9b076cc
YI FANG YUAN <yi.f.yuan@mail.mcgill.ca>
2021-04-28 15:17:43 -0400

    remove bootstrap

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -1,7 +1,4 @@
 import "./Experience.scss"
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 export default function Experience() {
 
     return (
