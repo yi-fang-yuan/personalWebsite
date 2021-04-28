bb97403f7d50916d4e73dd977c32ec76550dc244
YI FANG YUAN <yi.f.yuan@mail.mcgill.ca>
2021-04-28 15:12:21 -0400

    go

--- src/components/firstPage/Intro.js
+++ src/components/firstPage/Intro.js
@@ -17,7 +17,7 @@ export default function Intro() {
 
                 <div className="icons">
                     {/* <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/yi-fang-yuan-427378170/"><i className="fab fa-linkedin"></i></a> */}
-                    <a target="_blank" rel="noreferrer" href="https://github.com/yi-fang-yuan"><i class="fab fa-github"></i></a>
+                    {/* <a target="_blank" rel="noreferrer" href="https://github.com/yi-fang-yuan"><i class="fab fa-github"></i></a> */}
                     <a target="_blank" rel="noreferrer" href="https://www.instagram.com/yi_yifang/"><i class="fab fa-instagram"></i></a>
                     <a target="_blank" rel="noreferrer" href="https://www.facebook.com/yifang.yuan.3/"><i class="fab fa-facebook"></i></a>
                 </div>
